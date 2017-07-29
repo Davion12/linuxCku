7d4a10cfd56da707317f00dd2a2e4f938f352625
Devin <1206675950@qq.com>
2017-07-29 22:53:55 +0800

    更新Linux库函数操作

--- linuxCku.c
+++ linuxCku.c
@@ -2,16 +2,19 @@
 #include<stdio.h>
 #include<unistd.h>
 #include<stdlib.h>
+#include<time.h>
+#include<string.h>
 
-typedef struct Time{
+
+typedef struct Buffer{
 	char str[10];
 	int a[10];
-}time;
+}buffer;
 
-int main(int args, char* argv[])
+void calloc_function(void)
 {
 	//分配内存方法1
-	struct TIME *pr = calloc(sizeof(time), 10);
+	struct TIME *pr = calloc(sizeof(buffer), 10);
 	if(NULL == pr)
 	{
 		printf("alloca memory failed");
@@ -21,5 +24,32 @@ int main(int args, char* argv[])
 		printf("success alloca memory");
 	}
 	free(pr);
+
+	printf("the memory size is:%d",getpagesize());
+}
+
+void time_asc_print(void)
+{
+	//显示当前系统时间，以字符串的格式
+	time_t timesample;
+	time(&timesample);
+	printf("%s\n", asctime(gmtime(&timesample)));
+	printf("%s\n", ctime(&timesample));
+}
+
+void connectTwoString(void)
+{
+	//连接字符串操作
+	char str1[10] = "12345";
+	char str2[10] = "67890";
+	printf("%s", strcat(str1, str2));
+}
+
+
+
+int main(int args, char* argv[])
+{
+	
+	connectTwoString();
 	return 0;
 }
